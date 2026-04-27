a5a3ec46019b40c18459df4fab29a35df0b5fb9a
hadinnn <mhafizhmuhyiddin_2428240127@mhs.mdp.ac.id>
2026-04-27 13:12:40 +0700

    Buat Tabel Periode

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
+use App\Http\Controllers\PeriodeController;
 use App\Http\Controllers\FakultasController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-
+    return view('welcome');
 });
 
 Route::resource('fakultas', FakultasController::class);
+Route::resource('periode', PeriodeController::class);
