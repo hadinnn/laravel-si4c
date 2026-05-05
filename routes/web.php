83eb45e367d906f8b4afbaae485c27475b515fd0
nicosaputra <mhafizhmuhyiddin_2428240127@mhs.mdp.ac.id>
2026-05-05 12:53:25 +0700

    Menambahkan Tabel Prodi

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\BeritaController;
 use App\Http\Controllers\PeriodeController;
 use App\Http\Controllers\FakultasController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProdiController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -12,3 +13,4 @@ Route::get('/', function () {
 Route::resource('fakultas', FakultasController::class);
 Route::resource('periode', PeriodeController::class);
 Route::resource('berita', BeritaController::class);
+Route::get('/prodi', [ProdiController::class, 'index']);
